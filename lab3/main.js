7179628317d4376cb16fbc5b8670cb8f1e7f9aba
Adrian Dyjecinski <adriandyjecinski@gmail.com>
2019-04-28 07:09:04 +0200

    lab3 updated and lab5 added

--- lab3/main.js
+++ lab3/main.js
@@ -24,8 +24,8 @@ Weapon.prototype = {
     }
 }
 
-Weapon.prototype.message = function () {
-    console.log("Hello, this is weapon.")
+Weapon.prototype.shoot = function () {
+    console.log("Pow Pow Pow")
 }
 
 Weapon.prototype.addAmmo = function (ammoToAdd) {
@@ -42,11 +42,70 @@ function Shotgun (id, name, type, ammo, automatic, kickback) {
 Shotgun.prototype = Object.create(Weapon.prototype);
 Shotgun.prototype.constructor = Shotgun;
 
-Shotgun.prototype = {
-    getKickback: function () {
-        return this.kickback;
-    }
+function MachineGun (id, name, type, ammo, automatic, shootingSpeed) {
+    Weapon.call(this, id, name, type, ammo, automatic);
+    this.shootingSpeed = shootingSpeed;
 }
 
-var shotgunExample = new Shotgun(4321, "shotgun", "firearm", 4, false, 50);
-console.log(shotgunExample.getKickback);
+MachineGun.prototype = Object.create(Weapon.prototype);
+MachineGun.prototype.constructor = MachineGun;
+
+Shotgun.prototype.getKickback = function () {
+    return this.kickback;    
+}
+
+MachineGun.prototype.getShootingSpeed = function() {
+    return this.shootingSpeed;
+}
+
+// ----------------- Test -------------------
+
+var shotgun = new Shotgun(4321, "shotgun", "firearm", 4, false, 50);
+
+var machinegun = new MachineGun(4322, "machineGun", "firearm", 79, true, 300);
+
+console.log(shotgun.getKickback);
+console.log(machinegun.getShootingSpeed);
+
+var weaponBase = (function() {
+    weapons = []
+
+    var isWeaponExists = function(weapon) {
+        weapons.array.forEach(element => {
+            if (element == weapon) return true
+        })
+        return false
+    }
+
+    return {
+        addShotgun: function(id, name, type, ammo, automatic, kickback) {
+            var shotgun = new Shotgun(id, name, type, ammo, automatic, kickback)
+            if (isWeaponExists(shotgun)) {
+                console.log("Weapon already exists!")
+                return
+            }
+            weapons.push(shotgun)
+        },
+        addMachineGun: function(id, name, type, ammo, automatic, shootingSpeed) {
+            var machineGun = new MachineGun(id, name, type, ammo, automatic, shootingSpeed)
+            if (isWeaponExists(machineGun)) {
+                console.log("Weapon already exists!")
+                return
+            }
+            weapons.push(machineGun)
+        },
+        removeWeapons: function(weapon) {
+            var weaponId = weapons.indextOf(weapon)
+            weapons.slice(weaponId, 1)
+        },
+        getWeapons() {
+            return weapons
+        }
+    }
+})
+
+var database = weaponBase()
+
+var Ak47 = database.addMachineGun(4323, "Ak47", "firearm", 30, true, 600)
+
+console.log(database.getWeapons())
