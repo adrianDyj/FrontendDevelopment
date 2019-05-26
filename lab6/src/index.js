87857af5eb1d6051e497db057a9da71716e52279
Adrian Dyjecinski <adriandyjecinski@gmail.com>
2019-05-26 08:34:59 +0200

    Some changes

--- lab6/src/index.js
+++ lab6/src/index.js
@@ -1,6 +1,3 @@
-import { WeaponService } from "./service/weaponService"
-import { WeaponBuilder } from "./model/weaponBuilder"
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,30 +5,26 @@ const app = express();
 const port = 4000;
 
 app.use(bodyParser.json());
-
-const weaponBuilder = new WeaponBuilder()
-const weaponService = new WeaponService()
+const weapons = [];
 
 app.get('/api/', (req, res) => res.send('Hi there!!!'));
 
-app.get('/api/weapons', (req, res) => {
-    res.send(weaponService.getWeapons());
+app.get('/api/weapons/all', (req, res) => {
+    res.send(weapons);
 });
 
-app.get('/api/weapon/:weaponId', (req, res) => {
+app.get('/api/weapon', (req, res) => {
     res.send("Weapon has been added!");
 });
 
 app.post('/api/weapon', (req, res) => {
-    var weapon = weaponBuilder.buildWeapon(req)
-    var result = weaponService.addWeapon(weapon)
+    const weapon = req.body.weapon;
+    weapons.push(weapon);
     res.send({"Result": result});
 });
 
 app.delete('/api/weapon', (req, res) => {
-    var weapon = weaponBuilder.buildWeapon(req)
-    var result = weaponService.removeWeapon(weapon)
-    res.send({"Result": result});
+    res.send("Unsupported.");
 });
 
 app.listen(port, () => console.log('Example app listening on port ' + port));
